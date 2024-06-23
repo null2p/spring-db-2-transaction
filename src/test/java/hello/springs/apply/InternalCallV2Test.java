9b423449fe74540ac008bd2487d2e2f67159d729
nullpark <null.tmax@gmail.com>
2024-06-23 18:24:28 +0900

    [feat] 제대로 된 트랜잭션 사용 - 트랜잭션이 적용되지 않은 메소드에서 트랜잭션이 적용된 메소드를 이용하고 싶다면 클래스 분리

--- src/test/java/hello/springs/apply/InternalCallV2Test.java
+++ src/test/java/hello/springs/apply/InternalCallV2Test.java
@@ -1,5 +1,6 @@
 package hello.springs.apply;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,8 @@ public class InternalCallV2Test {
 
     @Autowired
     CallService callService;
+    @Autowired
+    InternalCallService internalCallService;
 
     @Test
     void printProxy() {
@@ -23,7 +26,7 @@ public class InternalCallV2Test {
 
     @Test
     void internallCall() {
-        callService.internal();
+        internalCallService.internal();
     }
 
     @Test
@@ -35,23 +38,40 @@ public class InternalCallV2Test {
     static class InternalCallV1TestConfig {
         @Bean
         CallService callService() {
-            return new CallService();
+            return new CallService(internalCallService());
+        }
+
+        @Bean
+        InternalCallService internalCallService() {
+            return new InternalCallService();
         }
     }
 
     @Slf4j
+    @RequiredArgsConstructor
     static class CallService {
+
+        private final InternalCallService internalCallService;
+
         public void external() {
             log.info("call external");
             printTxInfo();
-            internal();
+            internalCallService.internal();
         }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("txActive = {}", txActive);
+        }
+    }
+
+    static class InternalCallService {
+
         @Transactional
         public void internal() {
             log.info("call internal");
             printTxInfo();
         }
-
         private void printTxInfo() {
             boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
             log.info("txActive = {}", txActive);
