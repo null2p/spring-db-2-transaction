0052642da69901f7667152f14e5cc7e9b80c8cb0
nullpark <nuri_park2@tmax.co.kr>
2024-07-01 21:25:51 +0900

    [feat] orderService 내부의 독립적인 커밋, 롤백 - memberRepository : 정상 커밋 - logRepository : RuntimeException 으로 인해 롤백

--- src/test/java/hello/springs/propagation/MemberServiceTest.java
+++ src/test/java/hello/springs/propagation/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -33,6 +34,23 @@ class MemberServiceTest {
         //then
         Assertions.assertTrue(memberRepository.findByUsername(username).isPresent());
         Assertions.assertTrue(logRepository.findByUsername(username).isPresent());
+    }
+
+    /**
+     * memberService @Transactional : OFF
+     * memberRepository @Transactional : ON
+     * logRepository @Transactional : ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail";
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+        //then
+        Assertions.assertTrue(memberRepository.findByUsername(username).isPresent());
+        Assertions.assertTrue(logRepository.findByUsername(username).isEmpty());
     }
 
     @Test
